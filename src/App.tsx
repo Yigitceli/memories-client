3da85a41f563700f9d6a48553c13db764be5a752
Yigitceli <Yigit.celi@outlook.com>
2022-04-15 16:00:49 +0300

    Check refresh at start

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Auth from "./pages/Auth";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import store, { RootState, useAppDispatch } from "./redux/store";
 import Dashboard from "./pages/Dashboard";
-
+import axios from "axios";
 import { interceptor } from "./AxiosAuth";
+import { getRefreshToken } from "./utils";
+import { logOut } from "./redux/userSlice";
 
 function App() {
   const { data } = useSelector((state: RootState) => state.user);
-
+  const navigate = useNavigate()
   const dispatch = useAppDispatch();
+
+  const checkAuth = async () => {
+    try {
+      await axios.post(`http://localhost:5000/api/user/refresh-token`, {
+        refreshToken: getRefreshToken(),
+      });
+    } catch (error) {
+      dispatch(logOut())
+      navigate('/auth')
+    }
+  };
+
+  useEffect(() => {
+    checkAuth()
+  }, []);
 
   return (
     <div className="App p-4 relative w-100 h-screen flex flex-col ">
